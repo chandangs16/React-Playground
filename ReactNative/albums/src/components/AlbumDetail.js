d5c499de74154fbb2e32b82deb0cd2691b5d1274
CHANDAN GS <chandangs16@outlook.com>
2018-01-06 12:09:18 -0700

    added images

--- ReactNative/albums/src/components/AlbumDetail.js
+++ ReactNative/albums/src/components/AlbumDetail.js
@@ -5,12 +5,13 @@ import Card from './Card';
 import CardSection from './CardSection';
 
 const AlbumDetail = ({ album }) => {
-    const { title, artist } = album;
+    const { title, artist, thumbnail_image, image } = album;
     const { 
         thumbnailStyle, 
         headerContentStyle,
         thumbnailContainerStyle,
-        headerTextStyle
+        headerTextStyle,
+        imageStyle
     } = styles;
     return (
      <Card>
@@ -18,13 +19,17 @@ const AlbumDetail = ({ album }) => {
              <View style={thumbnailContainerStyle}>
                  <Image 
                     style={thumbnailStyle}
-                    source={{ uri: album.thumbnail_image }} 
+                    source={{ uri: thumbnail_image }} 
                  />
              </View>
              <View style={headerContentStyle}>
                 <Text style={headerTextStyle}> {title}</Text>
                 <Text> {artist}</Text>
              </View>
+         </CardSection>
+
+         <CardSection>
+             <Image style ={imageStyle} source={{ uri: image }} />
          </CardSection>
 
      </Card>
@@ -48,6 +53,11 @@ const styles = {
         alignItems: 'center',
         marginLeft: 10,
         marginRight: 10
+    },
+    imageStyle: {
+        height: 300,
+        flex: 1,
+        width: null
     }
 };
 
