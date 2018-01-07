87ec882165dde48489c37c204b62395086edfead
CHANDAN GS <chandangs16@outlook.com>
2018-01-06 18:32:30 -0700

    new project init with copying common common components

---
+++ ReactNative/auth/src/components/common/Button.js
@@ -0,0 +1,39 @@
+// Import a Library to help create a component
+import React from 'react';
+import { Text, TouchableOpacity } from 'react-native';
+
+const Button = ({ onPress, children }) => {
+    const { buttonSTyle, textStyle } = styles;
+    return (
+        <TouchableOpacity onPress={onPress} style={buttonSTyle}>
+         <Text style={textStyle}>
+            {children}
+         </Text>
+        </TouchableOpacity>
+    );
+};
+
+
+const styles = {
+    textStyle: {
+        alignSelf: 'center',
+        color: '#007aff',
+        fontSize: 16,
+        fontWeight: '600',
+        paddingTop: 10,
+        paddingBottom: 10
+    },
+    buttonSTyle: {
+        flex: 1, // expand to fill as much content as possible.
+        alignSelf: 'stretch',
+        backgroundColor: '#fff',
+        borderRadius: 5,
+        borderWidth: 1,
+        borderColor: '#007aff',
+        marginLeft: 5,
+        marginRight: 5
+
+    }
+};
+
+export default Button;
