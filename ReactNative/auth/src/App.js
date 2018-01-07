164b30640ac14c6f58e03cc0264d31870e69ca67
CHANDAN GS <chandangs16@outlook.com>
2018-01-06 19:10:07 -0700

    Firebase setup done for aLogin Authentication app.

--- ReactNative/auth/src/App.js
+++ ReactNative/auth/src/App.js
@@ -1,8 +1,21 @@
 import React, {Component} from 'react';
 import {View, Text} from 'react-native';
+import firebase from 'firebase';
 import {Header} from './components/common';
 
+
 class App extends Component {
+    componentWillMount() {
+        firebase.initializeApp({
+            apiKey: 'AIzaSyAu7H-Lpxsyav39-u-_LZ2g0VkJH_YC2wo',
+            authDomain: 'authentication-b7224.firebaseapp.com',
+            databaseURL: 'https://authentication-b7224.firebaseio.com',
+            projectId: 'authentication-b7224',
+            storageBucket: 'authentication-b7224.appspot.com',
+            messagingSenderId: '643786171758'
+          });
+    }
+
     render() {
         return (
             <View>
