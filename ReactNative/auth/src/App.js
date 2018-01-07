2fc370e929994a85c618a54529353d325839cef2
CHANDAN GS <chandangs16@outlook.com>
2018-01-07 01:18:28 -0700

    Handling Conditional Rendering

--- ReactNative/auth/src/App.js
+++ ReactNative/auth/src/App.js
@@ -1,11 +1,12 @@
 import React, {Component} from 'react';
 import {View, Text} from 'react-native';
 import firebase from 'firebase';
-import {Header} from './components/common';
+import {Header, Button, Spinner } from './components/common';
 import LoginForm from './components/LoginForm'
 
 
 class App extends Component {
+    state = {loggedIn: null};
     componentWillMount() {
         firebase.initializeApp({
             apiKey: 'AIzaSyAu7H-Lpxsyav39-u-_LZ2g0VkJH_YC2wo',
@@ -15,13 +16,44 @@ class App extends Component {
             storageBucket: 'authentication-b7224.appspot.com',
             messagingSenderId: '643786171758'
           });
+
+          firebase.auth().onAuthStateChanged((user) => {
+              if(user) {
+                  this.setState({loggedIn: true});
+              }
+              else {
+                  this.setState({loggedIn: false});
+              }
+
+          });
+    }
+
+    renderContent() {
+        switch(this.state.loggedIn){
+            case true:
+              return <Button> Log Out </Button> ;
+
+            case false:
+              return <LoginForm />;
+
+            default:
+              return <Spinner size="large" />;
+        }
+        // if(this.state.loggedIn){
+        //     return (
+        //         <Button>
+        //             Log Out
+        //         </Button>
+        //     );
+        // }
+        // return <LoginForm />
     }
 
     render() {
         return (
             <View>
                 <Header headerText="Authentication" />
-                <LoginForm />
+                {this.renderContent()}
             </View>
         );
     }
--- ReactNative/auth/src/components/LoginForm.js
+++ ReactNative/auth/src/components/LoginForm.js
@@ -21,7 +21,7 @@ class LoginForm extends Component {
     }
 
     onLoginFail() {
-        this.setState({error: 'Authentication Failed', loading: false})
+        this.setState({error: 'Authentication Failed', loading: false});
     }
 
     onLoginSuccess() {
