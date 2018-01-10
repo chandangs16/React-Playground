af17ac2249a9ed0705d78380c7a7873864eac4e7
CHANDAN GS <chandangs16@outlook.com>
2018-01-10 00:27:50 -0700

    move funstion out of components

--- ReactNative/tech_stack/src/components/ListItem.js
+++ ReactNative/tech_stack/src/components/ListItem.js
@@ -6,10 +6,19 @@ import * as actions from '../actions';
 
 
 class ListItem extends Component {
+
+    renderDescription() {
+        const { library, expanded } = this.props;
+        if (expanded) {
+            return (
+                <Text>{library.description}</Text>
+            );
+        }
+    }
     render() {
         const { titleStyle } = styles;
         const { id, title } = this.props.library;
-        console.log(this.props);
+        //console.log(this.props);
 
         return (
             <TouchableWithoutFeedback
@@ -21,6 +30,7 @@ class ListItem extends Component {
                             {title}
                         </Text>
                     </CardSection>
+                    {this.renderDescription()}
                 </View>
             </TouchableWithoutFeedback>
         );
@@ -35,4 +45,10 @@ const styles = {
     }
 };
 
-export default connect(null, actions)(ListItem);
+const mapStateToProps = (state, ownProps) => {
+    const expanded = state.selectedLibraryId === ownProps.library.id;
+    return { expanded };
+};
+
+
+export default connect(mapStateToProps, actions)(ListItem);
