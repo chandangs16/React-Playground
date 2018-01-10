52a4aa8b7af1d4d6332f154693cd7632ee219b00
CHANDAN GS <chandangs16@outlook.com>
2018-01-10 16:14:12 -0700

    new office manager app setup with firebase connect

---
+++ ReactNative/office/src/App.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import firebase from 'firebase';
+import reducers from './reducers';
+
+
+
+class App extends Component {
+
+    componentWillMount() {
+        const config = {
+            apiKey: "AIzaSyCLncHsCCOmKZy9RYc1VnT2fsgziRdjD98",
+            authDomain: "officemanager-c3d34.firebaseapp.com",
+            databaseURL: "https://officemanager-c3d34.firebaseio.com",
+            projectId: "officemanager-c3d34",
+            storageBucket: "officemanager-c3d34.appspot.com",
+            messagingSenderId: "1063818884981"
+          };
+          firebase.initializeApp(config);
+    }
+    render() {
+        return (
+            <Provider store={createStore(reducers)}>
+                <View>
+                    <Text>
+                        Hello!!
+                    </Text>
+                </View>
+            </Provider>
+        );
+    }
+}
+
+export default App;
