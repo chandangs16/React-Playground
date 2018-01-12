0d25129358d176b7cb5849160d670a6fab1ab611
CHANDAN GS <chandangs16@outlook.com>
2018-01-11 23:55:24 -0700

    added spinner

--- ReactNative/office/src/components/LoginForm.js
+++ ReactNative/office/src/components/LoginForm.js
@@ -2,7 +2,7 @@ import React, { Component} from 'react';
 import {View, Text} from 'react-native';
 import { connect} from 'react-redux';
 import { emailChanged, passwordChanged, loginUser } from '../actions';
-import {Card, CardSection, Input, Button} from './common';
+import {Card, CardSection, Input, Button, Spinner} from './common';
 
 class LoginForm extends Component {
 
@@ -31,6 +31,17 @@ class LoginForm extends Component {
         }
     }
 
+    renderButton() {
+        if(this.props.loading){
+            return <Spinner size="large" />;
+        }
+        return (
+            <Button onPress={this.onButtonPress.bind(this)}>
+                        Login
+            </Button>
+        );
+    }
+
     render() {
         return (
             <Card>
@@ -55,9 +66,8 @@ class LoginForm extends Component {
 
                 {this.renderError()}
                 <CardSection>
-                    <Button onPress={this.onButtonPress.bind(this)}>
-                        Login
-                    </Button>
+                    
+                    {this.renderButton()}
                 </CardSection>
             </Card>
         );
@@ -73,11 +83,10 @@ const styles = {
     }
 }
 
-const mapStateToProps = state => {
+const mapStateToProps = ({auth}) => {
+    const { email, password, error, loading } = auth;
     return {
-        email: state.auth.email,
-        password: state.auth.password,
-        error: state.auth.error
+        email, password, error, loading
     };
 };
 
