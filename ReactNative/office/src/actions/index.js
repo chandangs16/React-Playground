e963cb88c6a02c65ac35a7dcf7ae963a2a76b44c
CHANDAN GS <chandangs16@outlook.com>
2018-01-11 18:14:17 -0700

    before creating external dispatch function

--- ReactNative/office/src/actions/index.js
+++ ReactNative/office/src/actions/index.js
@@ -1,6 +1,9 @@
 import firebase from 'firebase';
-import {EMAIL_CHANGED} from './types';
-import {PASSWORD_CHANGED} from './types';
+import {
+    EMAIL_CHANGED,
+    PASSWORD_CHANGED,
+    LOGIN_USER_SUCCESS
+} from './types';
 
 export const emailChanged = (text) => {
     return {
@@ -20,7 +23,15 @@ export const loginUser = ({email,password}) => {
     return(dispatch) => {
         firebase.auth().signInWithEmailAndPassword(email, password)
             .then(user => {
-                dispatch({type: 'LOGIN_USER_SUCCESS', payload: user});
+                dispatch({type: LOGIN_USER_SUCCESS, payload: user});
+            })
+            .catch(() => {
+                firebase.auth().createUserWithEmailAndPassword(email,password)
+                .then(user => {
+                    dispatch({type: LOGIN_USER_SUCCESS, payload: user});
+                });
             });
     };
 };
+
+
