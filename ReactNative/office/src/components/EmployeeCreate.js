5053f86d1b505db14e945565be8502f62a438a38
CHANDAN GS <chandangs16@outlook.com>
2018-01-15 23:55:02 -0700

    added picker component

--- ReactNative/office/src/components/EmployeeCreate.js
+++ ReactNative/office/src/components/EmployeeCreate.js
@@ -1,4 +1,7 @@
 import React, {Component} from 'react';
+import { connect} from 'react-redux';
+import {Picker} from 'react-native';
+import { employeeUpdate } from '../actions';
 import { Card, CardSection, Input, Button } from './common';
 
 
@@ -10,30 +13,51 @@ class EmployeeCreate extends Component {
                     <Input
                         label="Name"
                         placeholder="Chandan"
+                        value={this.props.name}
+                        onChangeText={text => this.props.employeeUpdate({prop: 'name', value: text})}
                     />
                 </CardSection>
+                    
+                <CardSection>
                     <Input
                         label="Phone"
                         placeholder="480-410-2417"
+                        value={this.props.phone}
+                        onChangeText={value => this.props.employeeUpdate({ prop: 'phone', value })}
                     />
-                <CardSection>
-
                 </CardSection>
 
                 <CardSection>
-
+                    <Picker 
+                        style={{flex: 1}}
+                        selectedValue={this.props.shift}
+                        onValueChange={day => this.props.employeeUpdate({prop: 'shift', value: day})}
+                    >
+                        <Picker.Item label="Monday" value="Monday" />
+                        <Picker.Item label="Tuesday" value="Tuesday" />
+                        <Picker.Item label="Wednesday" value="Wednesday" />
+                        <Picker.Item label="Thursday" value="Thursday" />
+                        <Picker.Item label="Friday" value="Friday" />
+                        <Picker.Item label="Saturday" value="Saturday" />
+                        <Picker.Item label="Sunday" value="Sunday" />
+                    </Picker>
                 </CardSection>
-                <Button>
-                    Create
-                </Button>
+                
                 <CardSection>
-
+                    <Button>
+                        Create
+                    </Button>
                 </CardSection>
 
             </Card>
-
         );
     }
 }
 
-export default EmployeeCreate;
+const mapStateToProps = (state) => {
+    const { name, phone, shift } = state.employeeForm;
+
+    return { name, phone, shift };
+};
+
+export default connect(mapStateToProps, { employeeUpdate })(EmployeeCreate);
