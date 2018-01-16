f599e2926aafd01f88b3fc101bcf9bbaf2bbfc86
CHANDAN GS <chandangs16@outlook.com>
2018-01-15 23:59:51 -0700

    style override on picker

--- ReactNative/office/src/components/EmployeeCreate.js
+++ ReactNative/office/src/components/EmployeeCreate.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import { connect} from 'react-redux';
-import {Picker} from 'react-native';
+import {Picker, Text} from 'react-native';
 import { employeeUpdate } from '../actions';
 import { Card, CardSection, Input, Button } from './common';
 
@@ -27,7 +27,8 @@ class EmployeeCreate extends Component {
                     />
                 </CardSection>
 
-                <CardSection>
+                <CardSection style={{flexDirection: 'column'}}>
+                    <Text style={styles.pickerTextStyle}>Shift</Text>
                     <Picker 
                         style={{flex: 1}}
                         selectedValue={this.props.shift}
@@ -51,6 +52,13 @@ class EmployeeCreate extends Component {
 
             </Card>
         );
+    }
+}
+
+const styles = {
+    pickerTextStyle: {
+        fontSize: 20,
+        paddingLeft: 20
     }
 }
 
