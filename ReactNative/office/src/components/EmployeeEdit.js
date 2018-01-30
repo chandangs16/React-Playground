fd1bf4162265781e9ccc364c2f164b4c5e4d291f
CHANDAN GS <chandangs16@outlook.com>
2018-01-30 13:19:29 -0700

    final done

--- ReactNative/office/src/components/EmployeeEdit.js
+++ ReactNative/office/src/components/EmployeeEdit.js
@@ -3,7 +3,7 @@ import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import Communications from 'react-native-communications';
 import EmployeeForm from './EmployeeForm';
-import {employeeUpdate,employeeSave } from '../actions';
+import {employeeUpdate,employeeSave,employeeDelete } from '../actions';
 import {Card, CradSection, Button, CardSection,Confirm } from './common';
 
 class EmployeeEdit extends Component {
@@ -27,7 +27,8 @@ class EmployeeEdit extends Component {
     }
 
     onAccept() {
-
+        const {uid}= this.props.employee;
+        this.props.employeeDelete({uid});
     }
 
     onDecline() {
@@ -75,4 +76,5 @@ const mapStateToProps = (state) => {
 }
 
 export default connect(mapStateToProps,{
-    employeeUpdate,employeeSave})(EmployeeEdit);
+    employeeUpdate,employeeSave,employeeDelete
+})(EmployeeEdit);
