cfdb4930d5a954d64eba54611e4adf4cfb4c1053
CHANDAN GS <chandangs16@outlook.com>
2018-03-03 11:47:05 -0700

    added pan responder event handlers

--- ReactNative/swipe/src/Deck.js
+++ ReactNative/swipe/src/Deck.js
@@ -1,8 +1,28 @@
 
 import React, {Component} from 'react';
-import {View, Animated} from 'react-native';
+import {View, Animated,PanResponder} from 'react-native';
 
 class Deck extends Component {
+
+    constructor(props){
+        super(props);
+
+        const panResponder = PanResponder.create({
+            // executed anytime the user taps on the screen, any press down. if true, this
+            //intance of panresponder responsible for user touching the screen
+            onStartShouldSetPanResponder: () =>true,
+            //true callback. this callback is called when user drags on screen
+            // gesture has info on what user is doing on screen, what pixel value, 
+            //how quickly finger is moved
+            onPanResponderMove: (event, gesture) => {
+                console.log(gesture)
+            },
+            //when user clicks and relases touch
+            onPanResponderRelease: () => {}
+        })
+
+        this.state={panResponder};
+    }
 
     renderCards() {
         return this.props.data.map(item =>{
