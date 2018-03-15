3d961331c51dc66b34be6da080336a2ed180b911
CHANDAN GS <chandangs16@outlook.com>
2018-03-15 00:12:44 -0700

    animation changes

--- ReactNative/swipe/src/Deck.js
+++ ReactNative/swipe/src/Deck.js
@@ -7,6 +7,9 @@ class Deck extends Component {
     constructor(props){
         super(props);
 
+        const position = new Animated.ValueXY();
+
+
         const panResponder = PanResponder.create({
             // executed anytime the user taps on the screen, any press down. if true, this
             //intance of panresponder responsible for user touching the screen
@@ -15,17 +18,30 @@ class Deck extends Component {
             // gesture has info on what user is doing on screen, what pixel value, 
             //how quickly finger is moved
             onPanResponderMove: (event, gesture) => {
-                console.log(gesture)
+                //debugger
+                //console.log(gesture);
+                position.setValue({x: gesture.dx, y: gesture.dy });
             },
             //when user clicks and relases touch
             onPanResponderRelease: () => {}
-        })
+        });
+        //this.position=position;
 
-        this.state={panResponder};
+        this.state={panResponder, position};
     }
 
     renderCards() {
-        return this.props.data.map(item =>{
+        return this.props.data.map((item,index) =>{
+            if(index===0){
+                return (
+                    <Animated.View
+                        style={this.state.position.getLayout()}
+                        {...this.state.panResponder.panHandlers}
+                    >
+                        {this.props.renderCard(item)}
+                    </Animated.View>
+                )
+            }
             return this.props.renderCard(item);
         });
     }
@@ -36,7 +52,8 @@ class Deck extends Component {
             //panHandlers is an object that has bunch of callbacks that help
             //intercept presses from user
             //... spreads all the properties over the view
-            <View {...this.state.panResponder.panHandlers}>
+            //to tie panResponder to view add below property
+            <View>
                 {this.renderCards()}
             </View>
         );
