a1d0069ab6435c6aec1b4cc32adc347c53a464fd
CHANDAN GS <chandangs16@outlook.com>
2018-04-14 23:00:37 -0700

    final done

--- ReactNative/swipe/src/Deck.js
+++ ReactNative/swipe/src/Deck.js
@@ -60,6 +60,12 @@ class Deck extends Component {
         LayoutAnimation.spring();
     }
 
+    componentWillReceiveProps(nextProps){
+        if(nextProps.data!==this.props.data){
+            this.setState({index:0})
+        }
+    }
+
     forceSwipe(direction) {
         const x= direction === 'right'? SCREEN_WIDTH: -SCREEN_WIDTH;
         Animated.timing(this.state.position, {
